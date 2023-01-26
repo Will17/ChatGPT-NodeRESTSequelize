b0ef8d351f49eb5bda409c1a171eaffb4b257eba
Will17 <williampecoraro98@gmail.com>
2023-01-26 14:25:39 +0100

    first commit

--- index.js
+++ index.js
@@ -1,10 +1,52 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { Sequelize, Op } = require('sequelize');
+const { tableName } = require('./models');
+const fs = require('fs');
+const path = require('path');
 require('dotenv').config();
 
 const app = express();
 app.use(bodyParser.json());
+
+function validateAndSanitize(data, type="text") {
+  switch(type) {
+      case 'email':
+          data.email = validator.normalizeEmail(data.email);
+          if (!validator.isEmail(data.email)) {
+              throw new Error('Invalid email address');
+          }
+          break;
+      case 'numeric':
+          if (!validator.isNumeric(data.value)) {
+              throw new Error('Invalid numeric value');
+          }
+          break;
+      case 'percentage':
+          if (!validator.isFloat(data.value, { min: 0, max: 100 })) {
+              throw new Error('Invalid percentage value');
+          }
+          break;
+      case 'telephone':
+          data.telephone = validator.normalizePhoneNumber(data.telephone);
+          if (!validator.isMobilePhone(data.telephone)) {
+              throw new Error('Invalid telephone number');
+          }
+          break;
+      case 'text':
+          data.text = validator.trim(data.text);
+          if (!validator.isLength(data.text, { min: 1, max: 100 })) {
+              throw new Error('Invalid text value');
+          }
+          break;
+      default:
+          throw new Error('Invalid data type');
+  }
+
+  //return the sanitized data
+  return data;
+}
+
 
 // Connect to the MySQL database
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
@@ -19,26 +61,16 @@ const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, proces
 });
 
 // Define the models for the tables in the database
-const User = sequelize.define('user', {
-  name: Sequelize.STRING,
-  email: Sequelize.STRING
+const modelsPath = path.join(__dirname, 'models');
+fs.readdirSync(modelsPath).forEach(file => {
+  if (file.indexOf('.js') >= 0) {
+    const model = sequelize.import(path.join(modelsPath, file));
+  }
 });
-
-const Company = sequelize.define('company', {
-  name: Sequelize.STRING
-});
-
-// Define the associations between the models
-User.belongsTo(Company);
-Company.hasMany(User);
-
-// Sync the models with the database
-sequelize.sync();
 
 // Define the routes for the API
 app.get('/table/:name', async (req, res) => {
   try {
-    const tableName = req.params.name;
     let query = {
       attributes: ['*'],
       from: tableName
@@ -46,76 +78,75 @@ app.get('/table/:name', async (req, res) => {
 
     // Add a JOIN clause if specified in the query string
     if (req.query.join) {
-      query.include = [{
-        model: req.query.join,
-        required: true
-      }];
+      query.include = [
+        {
+          model: sequelize.models[req.query.join],
+          as: req.query.as
+        }
+      ];
     }
 
-    // Add a WHERE clause for filtering columns if specified in the query string
+    // Add a WHERE clause if specified in the query string
     if (req.query.filter) {
       const filter = JSON.parse(req.query.filter);
+      const {column, operator, value} = filter;
       query.where = {
-        [Op.and]: filter.map((condition) => {
-      let whereCondition = {};
-      whereCondition[condition.column] = {
-        [condition.operator]: condition.value
-      };
-      return whereCondition;
-    })
-  };
-}
+        [column]: {
+          [operator]: value
+        }
+      }
+    }
 
-const data = await sequelize.query(
-  'SELECT * FROM ' + tableName + ' WHERE ' + where + ';',
-  { type: sequelize.QueryTypes.SELECT }
-);
-
-res.json(data);
-        } catch (error) {
-    res.status(500).json({ error });
-  }
-});
-
-app.post('/table/:name', async (req, res) => {
-  try {
-    const tableName = req.params.name;
-    const data = req.body;
-    const model = tableName === 'users' ? User : Company;
-    await model.create(data);
-    res.json({ message: 'Data inserted successfully' });
+    // Find the data in the specified table
+    const data = await sequelize.models[tableName].findAll(query);
+    res.status(200).json(data);
   } catch (error) {
-    res.status(500).json({ error });
+    res.status(500).json({ message: error.message });
   }
 });
 
-app.put('/table/:name/:id', async (req, res) => {
-  try {
-    const tableName = req.params.name;
-    const id = req.params.id;
-    const data = req.body;
-    const model = tableName === 'users' ? User : Company;
-    await model.update(data, { where: { id } });
-    res.json({ message: 'Data updated successfully' });
-  } catch (error) {
-    res.status(500).json({ error });
-  }
+app.put('/:tableName/:id', (req, res) => {
+  // Validate user input and sanitize data
+  const id = req.params.id;
+  let updateData = req.body;
+  updateData = validateAndSanitize(updateData);
+
+  // Update the record in the database
+  model.update(updateData, { where: { id: id } }).then(() => {
+      res.status(200).send({ message: `Record with id ${id} updated successfully.` });
+  }).catch(err => {
+      res.status(500).send({ message: `Error updating record with id ${id}: ${err}` });
+  });
 });
 
-app.delete('/table/:name/:id', async (req, res) => {
-  try {
-    const tableName = req.params.name;
-    const id = req.params.id;
-    const model = tableName === 'users' ? User : Company;
-    await model.destroy({ where: { id } });
-    res.json({ message: 'Data deleted successfully' });
-  } catch (error) {
-    res.status(500).json({ error });
-  }
+app.post('/:tableName', (req, res) => {
+  // Validate user input and sanitize data
+  let newData = req.body;
+  newData = validateAndSanitize(newData);
+
+  // Insert the new record into the database
+  model.create(newData).then(record => {
+      res.status(201).send({ message: `Record created successfully with id: ${record.id}` });
+  }).catch(err => {
+      res.status(500).send({ message: `Error creating new record: ${err}` });
+  });
 });
 
+app.delete('/:tableName/:id', (req, res) => {
+  // Validate user input and sanitize data
+  const id = req.params.id;
+
+  // Delete the record from the database
+  model.destroy({ where: { id: id } }).then(() => {
+      res.status(200).send({ message: `Record with id ${id} deleted successfully.` });
+  }).catch(err => {
+      res.status(500).send({ message: `Error deleting record with id ${id}: ${err}` });
+  });
+});
+
+
+
+// Start the Express.js server
 app.listen(3000, () => {
   console.log('Server started on port 3000');
 });
-
-
